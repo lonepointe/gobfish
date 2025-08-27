f1ea08d17ed99e93996cc6a90adfd66d29ed9ebf
lonepointe <psycholabs@gmail.com>
2025-08-27 08:16:05 -0400

    testing

--- api/_env.php
+++
@@ -1,5 +0,0 @@
-<?php
-$ROOT = realpath(__DIR__ . '/..');
-if (!defined('DB_PATH')) define('DB_PATH', $ROOT . '/database/gobfish.db');
-if (!defined('TWITCH_CLIENT_ID')) define('TWITCH_CLIENT_ID', '6isq54n8of787rxzpoiuwlqq9jaq3k');
-
